ab26a6545fa8d3f658c165bdaa53a227d757696d
warunaman <137660511+warunaman@users.noreply.github.com>
2025-10-05 11:31:47 +1100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Eddit <code>src/App.js</code> and save to reload. Hello World v2!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>Welcome to React App - Hello Hello from Waruna</p>
+    </div>
+  );
+}
+
+export default App;
